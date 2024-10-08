a2d77c542ca7f94dfc908e53f84e36b9c526d651
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-10-08 23:30:34 +0300

    Day1 4th commit

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,4 +1,36 @@
+import { redis } from "../lib/redis.js";
 import User from "../model/user.model.js"
+import jwt from "jsonwebtoken"
+
+const generateTokens = (userId) =>{
+    const accessToken = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"15m",});
+
+    const refreshToken = jwt.sign({userId},process.env.REFRESH_TOKEN_SECRET , {expiresIn:"7d",})
+    
+    return {accessToken, refreshToken};
+};
+
+const storeRefreshToken = async(userId ,refreshToken) =>{
+    //7days
+    await redis.set(`refresh_token: ${userId}`,refreshToken,"EX",7*24*60*60) 
+}
+
+const setCookies = (res, accessToken , refreshToken) =>{
+
+    res.cookie("accessToken",accessToken , {
+        httpOnly:true,  //prevent XSS attacks , cross-site scripting attacks
+        secure : process.env.NODE_ENV ==="production",
+        sameSite:"strict",   //prevent CSRF attacks , cross-site request forgery
+        maxAge : 15 * 60 * 1000,  //15 minutes
+    }) 
+    
+    res.cookie("refreshToken",refreshToken ,{
+        httpOnly : true, 
+        secure : process.env.NODE_ENV === "production",
+        sameSite : "strict",
+        maxAge : 7 * 24 * 60 * 60 * 1000,   //7 days
+    })
+}
 
 export const signup = async (req, res)=>{
     const {name, email, password} = req.body;
@@ -13,7 +45,18 @@ export const signup = async (req, res)=>{
         {/*create user account */}
         const user = await User.create({name, email, password});
 
-        res.status(201).json({user, message:"user created successfully"});
+        {/*Authenticated */}
+        const {accessToken, refreshToken } = generateTokens(user._id);
+        await storeRefreshToken(user._id,refreshToken);
+        setCookies(res,accessToken,refreshToken);
+
+
+        res.status(201).json({user:{
+            _id :user._id,
+            name : user.name,
+            email : user.email,
+            role: user.role,
+        }, message:"user created successfully"});
     
      
    } catch (error) {
--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -45,25 +45,27 @@ const userSchema = new mongoose.Schema({
 })
 
 
-const User = mongoose.model("User" ,userSchema)
+
 
 //pre-save hook to hash password before saving to database
 
 userSchema.pre("save",async function(next){
-    if(!this.isModified("password")){
-        try {   
-            const salt = await bcrypt.genSalt(10);
-            this.password = await bcrypt.hash(this.password , salt)
-            next()
-        } catch (error) {
-            next(error)
-        }
-    }
+    if(!this.isModified("password")) return next();
+   
+    try {   
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password , salt)
+        next()
+    } catch (error) {
+        next(error)
+    }  
+    
 })
 
 userSchema.methods.comparePassword = async function (password){
     return bcrypt.compare(password, this.password)
 }
+const User = mongoose.model("User" ,userSchema)
 
 
 export default User;
