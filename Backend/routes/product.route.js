4d0f0017b6ac9657f926a5d2c455a922bb8e1613
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-10-11 20:51:24 +0300

    backend product controller finish

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { getAllFeaturedProducts, getAllProducts , createProduct } from "../controllers/product.controller.js";
+import { getAllFeaturedProducts, getAllProducts , createProduct, deleteProduct,getRecommendedProducts, getProductsCategory,toggleFeaturedProduct} from "../controllers/product.controller.js";
 import { protectRoute,adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express();
 
 router.get("/",protectRoute,adminRoute,getAllProducts)
 router.get("/featured" , getAllFeaturedProducts)
+router.get("/category/:category",getProductsCategory)
+router.get("/recommendations" , getRecommendedProducts)
+router.patch("/:id",protectRoute, adminRoute,toggleFeaturedProduct)
 router.post("/",protectRoute , adminRoute, createProduct)
+router.delete("/:id",protectRoute , adminRoute, deleteProduct)
 
 export default router;
