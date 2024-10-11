3b06c798477bd15226295361a8f118c09c66c434
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-10-12 00:15:48 +0300

    backend payment controller finish

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,8 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/payment.route.js"
+import analyticsRoutes from ""
 
 import { connectDB } from "./lib/db.js";
 
@@ -25,6 +27,8 @@ app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart",cartRoutes)
 app.use("/api/coupons",couponRoutes)
+app.use("/api/payments",paymentRoutes)
+app.use("/api/analytics",analyticsRoutes)
 
 
 
