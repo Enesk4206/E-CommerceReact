0b4c52a040d85ee93de5f35c49d59193a4e64782
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-10-11 21:47:02 +0300

    backend cart controller finish

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
+
 import { connectDB } from "./lib/db.js";
 
 {/*dotenv read .env extension codes procces.env */}
@@ -20,6 +22,7 @@ app.use(cookieParser());
 {/* http://localhost/api/auth/CURRENTROUTERADDRESS */}
 app.use("/api/auth",authRoutes)  
 app.use("/api/products",productRoutes)
+app.use("/api/cart",cartRoutes)
 
 
 
